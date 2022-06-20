cad0381387aea2630a7d9dbd53042f09644a215a
Anderson Keler <andersonkellerdev@hotmail.com>
2022-06-19 22:14:31 -0300

    Chess layer and print the board

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 
-import boardgame.Position;
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// Projected created
 		
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
